477fba7558c9dcbeac252e483cea6407ea77f4e9
soooonbin <dlvtoekf4@naver.com>
2025-02-24 20:50:29 +0900

    Add Frontend, Controller

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+    axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+  }, []);
+
+  return (
+      <div>
+        백엔드에서 가져온 데이터 : {hello}
+      </div>
+  );
+}
+
+export default App;
