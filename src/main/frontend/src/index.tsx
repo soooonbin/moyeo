11f670c0667e5945f511918645cb4b9a529fb850
GabyKim <gabik5566@gmail.com>
2025-03-09 14:25:25 +0900

    feat: Main화면, SelectRoom 화면 레이아웃 작성

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Provider from "./app/Router";
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
@@ -13,7 +13,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider />
     </React.StrictMode>
 );
-
+reportWebVitals();
